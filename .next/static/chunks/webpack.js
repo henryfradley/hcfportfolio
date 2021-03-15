54bb07d9f9122ef2283fb3cc9328b58cdf2a74c3
Henry Fradley <fradleyhenry@gmail.com>
2021-03-15 09:30:45 -1000

    fixed mobile hcflogo bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f6dcf9da6087895b3557";
+/******/ 	var hotCurrentHash = "7c7c3e551580fa6d32e9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
